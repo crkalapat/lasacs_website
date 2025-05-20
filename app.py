07e7088efa028124979cf49c2841e8e84e67c5ae
Charlie Kalapati <charlie.r.kalapati@gmail.com>
2025-05-20 18:01:09 -0500

    Add placeholder pages, create Ind Study page

--- app.py
+++ app.py
@@ -6,5 +6,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/ap')
+def ap():
+    return render_template('ap.html')
+
+@app.route('/acs')
+def acs():
+    return render_template('adv.html')
+
+@app.route('/de')
+def de():
+    return render_template('de.html')
+
+@app.route('/fun')
+def fun():
+    return render_template('fun.html')
+
+@app.route('/web')
+def web():
+    return render_template('web.html')
+
+@app.route('/is')
+def ind():
+    return render_template('is.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
